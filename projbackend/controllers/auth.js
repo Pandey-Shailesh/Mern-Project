e0828479ce355a9765d2dc7c7b9771d778a433b7
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-09-07 11:58:18 +0530

    create the signup route

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,3 +1,7 @@
+exports.signup = (req, res) => {
+  res.json({ message: "Signup works " });
+};
+
 exports.signout = (req, res) => {
   res.json({
     message: "User Signout",
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { signout, signup } = require("../controllers/auth");
 
-router.post("/singup", signup);
+router.post("/signup", signup);
 
 router.get("/signout", signout);
 
