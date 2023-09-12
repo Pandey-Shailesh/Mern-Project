5e8333744f8ffdf68a0c87420d54b121fd0db0bf
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-09-12 13:53:11 +0530

    create middleware  name as pushOrderInPurchaseList  to show the purchases in the array

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -11,8 +11,10 @@ const {
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 router.param("userId", getUserById);
+
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+
 router.get(
   "/orders/user/:userId",
   isSignedIn,
