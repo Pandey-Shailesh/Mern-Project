9a522db59bba860da4abdd82a23e77ecdfacaca3
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-09-14 11:40:15 +0530

    save the product into the data base and learn about form data

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
@@ -12,4 +15,41 @@ exports.getProductById = (req, res, next, id) => {
       req.product = product;
       next();
     });
+};
+
+exports.createProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Problem with Image",
+      });
+    }
+
+    //TODO: restrictionon field
+    let product = new Product(fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size is too bog!",
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    //save to the DB
+    product.save((err, product) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Saving course in Db failed",
+        });
+      }
+      res.json(product);
+    });
+  });
 };
