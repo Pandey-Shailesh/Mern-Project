63c65bcc46538c6bb6537544fc683452aad3889c
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-09-14 17:13:31 +0530

    create the routes of product ( getAllUniqueCategories and updateStock)

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 const formidable = require("formidable");
 const _ = require("lodash");
 const fs = require("fs");
+const category = require("../models/category");
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
@@ -143,4 +144,34 @@ exports.getAllProducts = (req, res) => {
       }
       res.json(products);
     });
+};
+
+exports.getAllUniqueCategories = (req, res) => {
+  Product.distinct("category", {}, (err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "No category found",
+      });
+    }
+    res.json(category);
+  });
+};
+
+exports.updateStock = (req, res, next) => {
+  let myOperations = req.body.order.products.map((prod) => {
+    return {
+      updateOne: {
+        filter: { _id: prod._id },
+        update: { $inc: { stock: -prod.count, sold: +prod.count } },
+      },
+    };
+  });
+  Product.bulkWrite(myOperations, {}, (err, products) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Bulk operation failed",
+      });
+    }
+    next();
+  });
 };
