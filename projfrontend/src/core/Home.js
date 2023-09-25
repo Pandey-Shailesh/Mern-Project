b9313eeb81a1456182e12bc2d546c054b7920117
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-09-25 11:05:57 +0530

    inject backend into frontend
    make sure the backend code is upend running

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles.css";
+import { API } from "../backend";
 
 export default function Home() {
+  console.log("API IS ", API);
   return (
     <div>
-      <h1 className="text-white"> Home Page Frontend</h1>
+      <h1 className="text-white"> Home Page Frontend </h1>
     </div>
   );
 }
