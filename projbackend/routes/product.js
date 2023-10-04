afdfb024396603196bd2b750f4fd98c3efd8b006
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-10-04 10:35:02 +0530

    done with Add to Card section
    and also solve the bug from backend

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -30,7 +30,7 @@ router.post(
 
 //read route
 router.get("/product/:productId", getProduct);
-router.get("/prouct/photo/:productId", photo);
+router.get("/product/photo/:productId", photo);
 
 //delete route
 router.delete(
