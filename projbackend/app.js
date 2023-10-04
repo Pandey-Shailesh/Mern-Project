6ee5844a07b8f5ccd199451b2f53b1a2e28fd43e
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-10-04 20:06:31 +0530

    remove all the stripe portion

--- projbackend/app.js
+++ projbackend/app.js
@@ -13,7 +13,6 @@ const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
-const stripeRoutes = require("./routes/stripepayment");
 
 //DB Connection
 mongoose
@@ -37,7 +36,6 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
-app.use("/api", stripeRoutes);
 
 
 //PORT
--- projbackend/routes/stripepayment.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-const { makepayment } = require("../controllers/stripepayment");
-router.post("/stripepayment", makepayment);
-module.exports = router;
